75e46dc0f59eb12314a2731291e50f27dc9c27cc
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-02 17:10:03 -1200

    Taking user input and printing output

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the User Registration Page");
+            Console.WriteLine("**********************************************");
+            ValidFirstName name = new ValidFirstName();
+            Console.WriteLine("Enter the First Name:");
+            string FName = Console.ReadLine();
+            Console.WriteLine(name.ValidateFirstName(FName));
         }
     }
 }
