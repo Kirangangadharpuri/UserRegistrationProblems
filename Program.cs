1dde26bc18bb6e3282ea6f18f7e7dac2fec991b5
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-02 17:21:27 -1200

    Taking user input and printing output

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace UserRegistration
+﻿using System.Runtime.InteropServices;
+
+namespace UserRegistration
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the User Registration Page");
+            ValidLastName name = new ValidLastName();
+            Console.WriteLine("Enter Last Name");
+            string LName = Console.ReadLine();
+            Console.WriteLine(name.ValidateLastName(LName));
         }
     }
 }
