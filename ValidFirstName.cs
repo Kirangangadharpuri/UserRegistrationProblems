e1e607d73ae75fe88991a6ec74352f5f9d9d063b
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-02 17:10:26 -1200

    validating the name as per requirements

---
+++ ValidFirstName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class ValidFirstName
+    {
+        public static string FirstName = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public bool ValidateFirstName(string FName)
+        {
+            return Regex.IsMatch(FName, FirstName);
+        }
+    }
+}
