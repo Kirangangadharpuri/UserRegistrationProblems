5eea6094662befbdaccff1f2d80a2bfca5bb1a01
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-02 17:57:18 -1200

    Taking user input and printing output

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the User Registration Page");
+            ValidMobileNumber Mobilenumber = new ValidMobileNumber();
+            Console.WriteLine("Please enter the mobile number:");
+            String mobilNumber= Console.ReadLine();
+            Console.WriteLine(Mobilenumber.ValidatMoNumber(mobilNumber));
         }
     }
 }
