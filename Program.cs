59c912f9d4c2dfe29c67730f9a4ab69691cdd28b
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-02 20:55:18 -1200

    Taking user input and printing output

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the User Registration Page");
+            PasswordRule1 Password = new PasswordRule1();
+            Console.WriteLine("Please enter password:");
+            string passWord = Console.ReadLine();
+            Console.WriteLine(Password.ValidatePassword(passWord));
         }
     }
 }
